a07ab2fa613782f92edcdb01381798909bb09884
kwonxogus <dvdv1313@gmail.com>
2024-05-02 22:08:48 +0900

    [240502] 템플릿 적용으로 인한 수정 및 추가

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -7,35 +7,28 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>KTH - LARAVEL</title>
-        <link href="assets/css/bootstrap.css" rel="stylesheet" />
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="assets/css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
         @yield('style')
         @yield('script')
     </head>
-    <body>
-        <div id="layoutAuthentication">
-            <div id="layoutAuthentication_content">
+    <body class="sb-nav-fixed">
+        @include('layout.topnav')
+        <div id="layoutSidenav">
+            @include('layout.sideNav')
+            <div id="layoutSidenav_content">
                 <main>
-                    @yield('content')
-                </main>
-            </div>
-            <div id="layoutAuthentication_footer">
-                <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; footer</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
+                        @yield('content')
                     </div>
-                </footer>
+                </main>
+                @include('layout.footer')
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="assets/js/scripts.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+        <script src="assets/js/datatables-simple-demo.js"></script>
     </body>
 </html>
