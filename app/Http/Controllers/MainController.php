cddd30dd9899906c760ae16eec6c6ced7c423970
kwonxogus <dvdv1313@gmail.com>
2024-05-05 23:18:15 +0900

    [240505] 게시판 테이블 db 적용

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MainController extends Controller
 {
     #main화면에서는 대시보드 편성해서 보여줄 것
     public function main(Request $request){
-        return view('main');
+        $list = DB::table('board_list')
+					->select('*')
+                    ->orderby('no')
+                    ->get();
+        return view('main')->with('list',$list);
     }
 
     #게시글 작성, 리스트, 좋아요, 댓글 만들 예정
